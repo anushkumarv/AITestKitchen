bdb50cb8ee61b891d88c8097c1fca8734a46c48a
anushkumarv <anush070@gmail.com>
2023-04-19 19:38:47 -0600

    added setup script for colab

---
+++ SAM/colab_setup.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# install python dependencies 
+pip install git+https://github.com/facebookresearch/segment-anything.git
+pip install opencv-python pycocotools matplotlib onnxruntime onnx
+
+# Create dirs 
+mkdir pretrained_models
+
+# Get SAM pre_trained model
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+mv sam_vit_h_4b8939.pth ./pretrained_models/.
+
+# Get validation images from answer grounding shared task
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/images/val.zip
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/VizWiz_grounding/annotations.zip
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/VizWiz_grounding/create_binary_masks.py
+unzip -q val.zip 
+unzip -q annotations.zip
