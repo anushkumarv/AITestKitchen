425cc14180d85ddd21da5f45516929d66d2ec334
anushkumarv <anush070@gmail.com>
2023-04-20 00:29:44 -0600

    Folder for predictions

--- SAM/colab_setup.sh
+++ SAM/colab_setup.sh
@@ -6,6 +6,7 @@ pip install opencv-python pycocotools matplotlib onnxruntime onnx
 
 # Create dirs 
 mkdir pretrained_models
+mkdir predictions
 
 # Get SAM pre_trained model
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
